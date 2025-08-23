85aef358356f75a883abb73442840322a28d022d
Natasha2702 <126653681+Natasha2702@users.noreply.github.com>
2025-08-23 18:39:06 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo-shecodes.png';
+import './App.css';
+
+export default function App() {
+  return (
+<div className='App-container'>
+<div className="App">
+      <header className="App-header">
+        <img src={logo} className='App-header img-fluid'/>
+      </header>
+      <main>
+
+      </main>
+      <footer className='App-footer text-center'>
+        coded by Natasha
+      </footer>
+    </div>
+    </div>
+  );
+}
+
