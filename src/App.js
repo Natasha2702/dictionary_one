c2d8da5c737411311a2c5fc2ad5e846c088505e2
Natasha2702 <126653681+Natasha2702@users.noreply.github.com>
2025-08-25 14:04:47 +0200

    added dictionary api integration

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import logo from './logo-shecodes.png';
 import './App.css';
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
 <div className='App-container'>
 <div className="App">
       <header className="App-header">
-        <img src={logo} className='App-header img-fluid'/>
+        <img src={logo} className='App-header img-fluid' alt="logo"/>
       </header>
       <main>
-
+      <Dictionary/>
       </main>
       <footer className='App-footer text-center'>
         coded by Natasha
