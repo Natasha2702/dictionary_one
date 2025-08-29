22dc7a7e76565eaa72239bd8b3147448b7d6cfbd
Natasha2702 <126653681+Natasha2702@users.noreply.github.com>
2025-08-29 17:53:58 +0200

    Added Phonetics

---
+++ src/Phonetic.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+export default function Phonetic(props) {
+  function playAudio() {
+    const audio = new Audio(props.phonetic.audio);
+    audio.play();
+  }
+
+  return (
+    <div className="Phonetic">
+      <button onClick={playAudio} className="listen-button">
+        Listen
+      </button>
+      <span className="text">{props.phonetic.text}</span>
+    </div>
+  );
+}
